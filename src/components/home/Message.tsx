19a51ee3f9336801ffdfd3c1cb35dd14b32fb03f
magedElshafey <magedelshafey98@gmail.com>
2025-03-12 11:56:29 +0200

    updates

--- src/components/home/Message.tsx
+++ src/components/home/Message.tsx
@@ -5,9 +5,10 @@ interface MessageProps {
   data: About[];
   hasDesc? : boolean
 }
-const Message: React.FC<MessageProps> = ({ data , hasDesc }) => {
+const Message: React.FC<MessageProps> = ({ data, hasDesc }) => {
+  // grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  justify-center mx-auto
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-6 lg:gap-8 xl:gap-12">
+    <div className="flex flex-wrap items-center justify-start md:justify-center gap-4 md:gap-6 lg:gap-8 xl:gap-12">
       {data?.map((item: About, index: number) => (
         <motion.div
           key={index}
@@ -16,7 +17,7 @@ const Message: React.FC<MessageProps> = ({ data , hasDesc }) => {
           whileHover={{ scale: 1.2 }}
           viewport={{ once: false, amount: 0.2 }} 
           transition={{ duration: 0.6, ease: "easeOut", delay: index * 0.1 }}
-          className="bg-custom-gradient p-3 flex items-center justify-center rounded-3xl  text-white text-center  min-h-[180px]   cursor-pointer"
+          className="bg-custom-gradient p-3 flex items-center justify-center rounded-3xl  text-white text-center  min-h-[180px] w-[150px] md:w-[220px] lg:w-[250px]  cursor-pointer"
         >
           <div>
           <p className={hasDesc ? "mb-3" : ""}>{item?.name}</p>
